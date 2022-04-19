e6e3f528ddb07b78f779ebaa0d9afc82fa1bc38f
cedcoss <amnakumarsrivastava@cedcoss.com>
2022-04-19 12:31:09 +0530

    orders

--- src/app/controllers/OrdersController.php
+++ src/app/controllers/OrdersController.php
@@ -8,13 +8,37 @@ class OrdersController extends Controller
 
     public function indexAction()
     {
-        echo "hi";
-        die;
+        $collection = $this->mongo->orders;
+        $cursor = $collection->find()->toArray();
+        $this->view->data = $cursor;
     }
 
     public function addAction()
     {
-       $this->view->hola='hola';
-     
+        $collection = $this->mongo->products;
+        $cursor = $collection->find()->toArray();
+        $this->view->data = $cursor;
+
+
+        if ($this->request->ispost()) {
+            print_r($this->request->getPost());
+    
+
+
+            $data = [
+                'customer_name' => $this->request->getPost('cname'),
+                'qty' => $this->request->getPost('qty'),
+                'address' => $this->request->getPost('address'),
+                'prod' => $this->request->getPost('prod'),
+                'status'=>'Paid'
+            ];
+
+    
+            $collection = $this->mongo->orders;
+            $insertOneResult = $collection->insertOne($data);
+            $this->response->redirect("/orders");
+        }
+
+
     }
 }
